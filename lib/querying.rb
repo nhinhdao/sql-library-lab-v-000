902fc1946c4dc0ad3b63ff003390362631ce4033
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-07-24 01:27:47 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "select species, count(species) from characters order by count(species) desc limit 1";
 end
 
 def select_name_and_series_subgenres_of_authors
