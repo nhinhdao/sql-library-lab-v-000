b62d49ca2f0e80fb44f15bef0f60a58331199da6
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-07-24 00:48:56 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "select series.title from series inner join characters on series.id = characters.series_id where characters.species = 'human' order by characters.title limit 1"
+  "select series.title from series inner join characters on series.id = characters.series_id where characters.species = 'human' order by series.title limit 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
